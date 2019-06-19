86606d6a69c2f7b0892acc2178b5493e828d7ca2
Maciek Kwiatkowski <kwiatkowskimack@gmail.com>
2019-06-19 14:55:23 +1000

    added validations for creating a user

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const router = express.Router()
+const {check, validationResult} = require('express-validator/check');
 
-router.post('/', (req, res) => {
-  res.send('Registers a user')
+const User = require('../models/User')
+
+router.post('/', [
+  check('name', 'Please add name')
+    .not()
+    .isEmpty(),
+  check('email', 'Please include valid email').isEmail(),
+  check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
+], (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res
+      .status(400)
+      .json({
+        errors: errors.array()
+      })
+  }
+  res.send('passed')
 })
 
 module.exports = router
