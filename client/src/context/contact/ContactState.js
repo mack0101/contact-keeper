a49f82471199936a24c46afc594d79c712112921
Maciek Kwiatkowski <kwiatkowskimack@gmail.com>
2019-06-20 15:01:08 +1000

    added components and started on context api

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,69 @@
+import React, {useReducer} from 'react';
+import uuid from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER
+} from '../types';
+
+const ContactState = props => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Person One',
+        email: 'person@one.test',
+        phone: '111-111-1121',
+        type: 'personal'
+      },
+      {
+        id: 2,
+        name: 'Person Two',
+        email: 'person@two.test',
+        phone: '111-111-1221',
+        type: 'personal'
+      },
+      {
+        id: 3,
+        name: 'Person Three',
+        email: 'person@three.test',
+        phone: '111-111-1321',
+        type: 'professional'
+      }
+    ]
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  // add contact
+
+  // delete contact
+
+  // set current contact
+
+  // clear current contact
+
+  // update contact
+
+  // filter contacts
+
+  // clear filter
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
