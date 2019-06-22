ccd6b99d3eab6746a77fcb233dafe656e6f0f97e
Maciek Kwiatkowski <kwiatkowskimack@gmail.com>
2019-06-22 18:38:20 +1000

    added option to edit contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -36,7 +36,8 @@ const ContactState = props => {
         phone: '111-111-1321',
         type: 'professional'
       }
-    ]
+    ],
+    current: null
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -53,10 +54,19 @@ const ContactState = props => {
   };
 
   // set current contact
+  const setCurrent = contact => {
+    dispatch({type: SET_CURRENT, payload: contact});
+  };
 
   // clear current contact
+  const clearCurrent = () => {
+    dispatch({type: CLEAR_CURRENT});
+  };
 
   // update contact
+  const updateContact = contact => {
+    dispatch({type: UPDATE_CONTACT, payload: contact});
+  };
 
   // filter contacts
 
@@ -66,8 +76,12 @@ const ContactState = props => {
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
+        current: state.current,
         addContact,
-        deleteContact
+        deleteContact,
+        setCurrent,
+        clearCurrent,
+        updateContact
       }}
     >
       {props.children}
